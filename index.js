36fef41a53434035b42aadee8334b0e5e78f5859
Mahbubur Rahman <mail@mahbub.me>
2018-12-19 09:04:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,11 +21,3 @@ function sayHiToGrandma( string ) {
     return "I can't hear you!";
   }
 }
-
-function sayHiToGrandma( string ) {
-  var uppercase = 'HELLO';
-  
-  if( string.toUpperCase() === uppercase ) {
-    return "YES INDEED";    
-  }
-}
